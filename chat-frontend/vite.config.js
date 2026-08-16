a0d23e8c1dfcef91d93741543d5897ae2b611e00
Agastya Nath <agastyanath1@gmail.com>
2026-08-16 22:49:10 +0530

    New additions

--- chat-frontend/vite.config.js
+++ chat-frontend/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from "fs";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  server: {
+    host: "0.0.0.0",
+    port: 6000,
+
+    https: {
+      key: fs.readFileSync("/home/student/chat-ssl/key.pem"),
+      cert: fs.readFileSync("/home/student/chat-ssl/cert.pem"),
+    },
+  },
+});
